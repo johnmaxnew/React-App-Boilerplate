c2393e00788178dde552571b87bd24d65e0fb97a
johnmaxnew <forevermeticulous@gmail.com>
2021-10-10 23:59:29 +0300

    modified components

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,12 +2,33 @@ import React from 'react'
 
 function Home() {
     return (
-        <div className="panel">
-            <h3>
-                Home
-                <small class="text-muted">This is home content</small>
-            </h3>
+
+
+        <div id="carouselExampleFade" class="carousel slide carousel-fade" data-bs-ride="carousel">
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+            <img src="https://source.unsplash.com/user/erondu/1600x900" class="d-block w-100" alt="..."/>
+            </div>
+            <div class="carousel-item">
+            <img src="https://source.unsplash.com/user/erondu/1600x900" class="d-block w-100" alt="..."/>
+            </div>
+            <div class="carousel-item">
+            <img src="https://source.unsplash.com/user/erondu/1600x900" class="d-block w-100" alt="..."/>
+            </div>
         </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+        </button>
+        </div>
+
+
+
+
     )
 }
 
